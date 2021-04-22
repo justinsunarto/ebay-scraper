02a9c6ca6b885a51c9999db4ec6f4acdb014c0f1
Justin Sunarto <justinever9@gmail.com>
2021-04-23 00:16:33 +0700

    edit finisher.rb back to normal

--- finisher/finisher.rb
+++ finisher/finisher.rb
@@ -1,4 +1,4 @@
-# require "dh_easy/qa"
+require "dh_easy/qa"
 
 collections = Datahen::Client::ScraperJobOutput.new.collections("ebay")
 collection = collections.find{|collection| collection['collection'] == "listings" }
@@ -12,5 +12,5 @@ else
    puts "no listings collection found"
 end
 
-# vars = {"scraper_name" => "ebay", "collections" => ["listings"]}
-# DhEasy::Qa::Validator.new.validate_internal(vars, outputs)
+vars = {"scraper_name" => "ebay", "collections" => ["listings"]}
+DhEasy::Qa::Validator.new.validate_internal(vars, outputs)
