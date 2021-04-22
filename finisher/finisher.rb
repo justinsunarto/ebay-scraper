382700ff610a9b6d34e8b4831237a0337686f66a
Justin Sunarto <justinever9@gmail.com>
2021-04-22 23:45:17 +0700

    added qa to listings in finisher script

--- finisher/finisher.rb
+++ finisher/finisher.rb
@@ -1,11 +1,16 @@
+require "dh_easy/qa"
+
 collections = Datahen::Client::ScraperJobOutput.new.collections("ebay")
 collection = collections.find{|collection| collection['collection'] == "listings" }
 if collection
   total = collection["outputs"]
-   outputs << {
-     "_collection" => "summary",
-     "total_listings" => total
-   }
+    outputs << {
+        "_collection" => "summary",
+        "total_listings" => total
+    }
 else
    puts "no listings collection found"
 end
+
+vars = {"scraper_name" => "ebay", "collections" => ["listings"]}
+DhEasy::Qa::Validator.new.validate_internal(vars, outputs)
